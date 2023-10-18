94fef9cf5217ed34892df299eb0f052c766a5756
Wolf <wolfverelst@gmail.com>
2023-10-18 14:32:19 +0200

    feat(lab3): play random bird sound from api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ export default class App {
     constructor() {
         console.log('Constructed!');
         this.getLocation();
+        this.setupBirdSoundButton();
     }
 
     getLocation() {
@@ -18,6 +19,20 @@ export default class App {
         this.getWeather(x, y);
     }
 
+    updateWeatherIcon(temperature) {
+        const weatherIcon = document.querySelector(".weather-icon");
+    
+        if (temperature < 5) {
+          weatherIcon.className = "fas fa-snowflake"; // Snowflake icon
+        } else if (temperature < 12) {
+          weatherIcon.className = "fas fa-snowflake"; // Cold icon
+        } else if (temperature < 20) {
+          weatherIcon.className = "fas fa-cloud"; // Cloud icon
+        } else {
+          weatherIcon.className = "fas fa-sun"; // Sun icon
+        }
+      }
+
     getWeather(x, y) {
         //api key url to ask for all data needed with x and y coordinates
         fetch(`https://api.open-meteo.com/v1/forecast?latitude=${x}&longitude=${y}&hourly=temperature_2m&current_weather=true&forecast_days=1`)
@@ -26,14 +41,45 @@ export default class App {
                 const temp = data.current_weather.temperature;
                 document.querySelector("h2").innerHTML = temp + "°C";
 
-                if (temp < 12) {
-                    console.log("cold");
-                }
-            })
+                this.updateWeatherIcon(temp);
+            }
+            
+            )
+
+
+
+            
             .catch(error => console.log(error));
     }
 
     showError(error) {
         console.log(error);
     }
+
+
+// fetch and play a random bird sound from Brazil
+  playRandomBirdSound() {
+    // make a request to the Xeno-Canto API to get a list of recordings from Brazil
+    fetch("https://xeno-canto.org/api/2/recordings?query=cnt:brazil&page=5")
+      .then(response => response.json())
+      .then(data => {
+
+        const randomIndex = Math.floor(Math.random() * data.recordings.length);
+        const randomRecording = data.recordings[randomIndex];
+
+        const audioUrl = randomRecording.file;
+
+        const audio = new Audio(audioUrl);
+        audio.play();
+      })
+      .catch(error => console.log(error));
+  }
+
+  setupBirdSoundButton() {
+    const playButton = document.getElementById("play-bird-sound");
+    playButton.addEventListener("click", () => {
+      this.playRandomBirdSound();
+    });
+  }
+
 }
